e9185c12361f94227c872011b6e34cd9be4e975b
Llbes98 <113596722+Llbes98@users.noreply.github.com>
2022-09-29 12:52:52 +0200

    har startet på at implementere lamper og få dem til at skifte med levels

--- Simons_game/Simons_game.ino
+++ Simons_game/Simons_game.ino
@@ -1,4 +1,4 @@
-#include <LiquidCrystal_I2C.h> // I2C address: 0x27
+//#include <LiquidCrystal_I2C.h> // I2C address: 0x27
 
 // set the LCD number of columns and rows
 int lcdColumns = 16;
@@ -6,15 +6,15 @@ int lcdRows = 2;
 
 // set LCD address, number of columns and rows
 // if you don't know your display address, run an I2C scanner sketch
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
+//LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 
 //Pin constants
-/*const int redLed = x;
-const int greenLed = x;
-const int blueLed = x;
-const int yellowLed = x;
+const int redLed = D5;
+const int yellowLed = D6;
+const int greenLed = D7;
+const int blueLed = D8;
 
-const int redLedButton = x;
+/*const int redLedButton = x;
 const int greenLedButton = x;
 const int blueLedButton = x;
 const int yellowLedButton = x;
@@ -33,6 +33,10 @@ int levels[31];
 
 void setup() {
   Serial.begin(9600);
+  pinMode(redLed, OUTPUT);
+  pinMode(yellowLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(blueLed, OUTPUT);
   
   //Initialize array
   for(int i = 0; i<sizeof(levels)/2; i++) {
@@ -94,6 +98,34 @@ void setupGamemode1() {
     Serial.println(difficulty);
     generateLevels(4);
   }
+
+
+for (int i = 0; i < skillLevel[difficulty-49]; i++) {
+  if (levels[i] == 1) {
+    digitalWrite(redLed, HIGH);
+    delay(500);
+    digitalWrite(redLed, LOW);
+    delay(500);
+  } else if (levels[i] == 2) {
+    digitalWrite(yellowLed, HIGH);
+    delay(500);
+    digitalWrite(yellowLed, LOW);
+    delay(500);
+  } else if (levels[i] == 3) {
+    digitalWrite(greenLed, HIGH);
+    delay(500);
+    digitalWrite(greenLed, LOW);
+    delay(500);
+  } else if (levels[i] == 4) {
+    digitalWrite(blueLed, HIGH);
+    delay(500);
+    digitalWrite(blueLed, LOW);
+    delay(500);
+  } else if (levels[i] == -1) {
+    break;
+  }
+}
+  
 }
 
 void generateLevels(int x) {
